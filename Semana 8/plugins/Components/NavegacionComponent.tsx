f9313528b105df236041504f42bfd68562fc7bd1
errapalog <errapalog@bancatlan.hn>
2024-11-26 20:54:44 -0600

    plugin

---
+++ Semana 8/plugins/Components/NavegacionComponent.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import Acelerometro from '../page/Acelerometro'
+import Camara from '../page/Camara'
+import Mapa from '../page/Mapa'
+
+export default function NavegacionComponent() {
+
+  const tabNavigation = createBottomTabNavigator()
+  return (
+    
+    <NavigationContainer>
+
+        <tabNavigation.Navigator initialRouteName='Acelerometro'>
+            <tabNavigation.Screen name='Acelerometro' component={Acelerometro}></tabNavigation.Screen>
+            <tabNavigation.Screen name='Camara' component={Camara}></tabNavigation.Screen>
+            <tabNavigation.Screen name='Mapa' component={Mapa}></tabNavigation.Screen>
+        </tabNavigation.Navigator>
+    </NavigationContainer>
+  )
+}
